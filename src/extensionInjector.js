f2bf2782f849b5a9f0eb4dfc5c472e88feac37a2
Dima Vishnevetsky <dimshik100@gmail.com>
2020-05-17 19:00:53 +0300

    updated injector to inject only into supported url

--- src/extensionInjector.js
+++ src/extensionInjector.js
@@ -7,5 +7,34 @@ function addScript(src) {
   (document.body || document.head || document.documentElement).appendChild(script);
 }
 
-// Important to add the script to the DOM only after we registered the event listeners
-addScript("extension.js");
+const getStorageData = (key) =>
+  new Promise((resolve, reject) =>
+    chrome.storage.sync.get(key, (result) =>
+      chrome.runtime.lastError
+        ? reject(Error(chrome.runtime.lastError.message))
+        : resolve(result)
+    )
+  );
+
+  // check if current website is in the supported urls list
+  async function checkIfUrlMatches() {
+    const storageKey = "scrollPercentageUrls";
+    let data = await getStorageData(storageKey);
+    console.log("checkIfUrlMatches -> data", data);
+
+    const urls = data[storageKey];
+
+    if(!urls) {
+      return;
+    }
+
+    for (const url of urls) {
+      if (window.location.href.includes(url)) {
+        addScript("extension.js");
+        return;
+      }
+    }
+  }
+
+
+  checkIfUrlMatches();
