a3b2eade30228d69e3582ef63e4fddc600722991
Dima Vishnevetsky <dimshik100@gmail.com>
2020-05-17 19:00:00 +0300

    Updated popup to read/write to chrome sync storage

--- src/popup.js
+++ src/popup.js
@@ -1,41 +1,123 @@
 const urlsList = document.getElementById("urlsList");
 const form = document.getElementById("addUrlForm");
 const urlInput = document.getElementById("url");
+const resetStorageBtn = document.getElementById("resetStorage");
 
 const storageKey = "scrollPercentageUrls";
+const defaultUrls = ["medium.com", "dev.to"];
 
-chrome.storage.sync.get([storageKey], function (result) {
-  console.log("Value currently is " + result.key);
-});
+const getStorageData = (key) =>
+  new Promise((resolve, reject) =>
+    chrome.storage.sync.get(key, (result) =>
+      chrome.runtime.lastError
+        ? reject(Error(chrome.runtime.lastError.message))
+        : resolve(result)
+    )
+  );
 
-// temp urls list
-urls = ["asd.com", "asdsadsad.com"];
+const setStorageData = (data) =>
+  new Promise((resolve, reject) =>
+    chrome.storage.sync.set(data, () =>
+      chrome.runtime.lastError
+        ? reject(Error(chrome.runtime.lastError.message))
+        : resolve()
+    )
+  );
 
-let urlListItemsHtml = "";
+const clearStorageData = () =>
+  new Promise((resolve, reject) =>
+    chrome.storage.sync.clear(() =>
+      chrome.runtime.lastError
+        ? reject(Error(chrome.runtime.lastError.message))
+        : resolve()
+    )
+  );
 
-for (const url of urls) {
-  urlListItemsHtml += `<li>${url} <button class="delete">Del</button></li>`;
-}
+refreshUrls();
 
-urlsList.innerHTML = urlListItemsHtml;
-
-function addUrl(event) {
+async function addUrl(event) {
   console.log("submit -> event", event);
   event.preventDefault();
 
   const url = urlInput.value;
 
+  // if the input field is empty, don't do anything
   if (!url) {
     return;
   }
 
-  urls.push(url);
+  // get latest data from storage
+  let data = await getStorageData(storageKey);
+  console.log("addUrl -> data", data)
   
-  chrome.storage.sync.set({ storageKey: urls }, function () {
-    console.log("Value is set to " + urls);
-  });
+  const urls = data[storageKey];
+
+  // make sure we don't have this url already in the list
+  if (urls.includes(url)) {
+    return;  
+  }
+
+  urls.push(url);
+
+  await setStorageData({ scrollPercentageUrls: urls })
+
+  // clear input value
+  urlInput.value = "";
+
+  refreshUrls();
 }
 
-function removeUrl(event) {}
+async function removeUrl(url) {
+   // get latest data from storage
+   let data = await getStorageData(storageKey);
+   console.log("removeUrl -> data", data)
+   
+   const currentUrls = data[storageKey];
+
+   const urls  = currentUrls.filter(e => e !== url);
+
+   await setStorageData({ scrollPercentageUrls: urls })
+
+   refreshUrls();
+}
+
+async function refreshUrls() {
+  let urlListItemsHtml = "";
+
+  let data = await getStorageData(storageKey);
+  console.log("refreshUrls -> data", data);
+
+  // if there is no urls in storage, set some default urls
+  if (!data[storageKey]) {
+    console.log("refreshUrls -> Setting defaultUrls");
+    await setStorageData({ scrollPercentageUrls: defaultUrls })
+    data = await getStorageData(storageKey);
+  }
+
+  console.log("refreshUrls -> data", data)
+  const urls = data[storageKey];
+
+  for (const url of urls) {
+    urlListItemsHtml += `<li>${url} <button class="delete-btn" data-url="${url}">Del</button></li>`;
+  }
+
+  urlsList.innerHTML = urlListItemsHtml;
+}
+
+async function resetStorage() {
+  await clearStorageData();
+  refreshUrls();
+}
 
 form.addEventListener("submit", addUrl, true);
+resetStorageBtn.addEventListener("click", resetStorage);
+
+urlsList.addEventListener("click", (event)=>{
+  let target = event.target;
+
+  if (!target.classList.contains("delete-btn")) {
+    return;
+  }
+
+  removeUrl(target.dataset.url);
+});
