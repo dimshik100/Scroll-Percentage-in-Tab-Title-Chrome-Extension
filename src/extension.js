27f62336f4ea3b922f4703ca752d68ea7fd900b2
Moshe Harush <mosheh@yad2.co.il>
2020-06-02 13:37:09 +0300

    Try find a main conainer by Common selectors

--- src/extension.js
+++ src/extension.js
@@ -4,11 +4,24 @@ console.log("Scroll Percentage in Tab Title Extension loaded 👍");
 
 const originalTitle = document.title;
 
-window.addEventListener("scroll", () => {
-  let scrollTop = window.scrollY;
-  let docHeight = document.body.offsetHeight;
-  let winHeight = window.innerHeight;
-  let scrollPercent = scrollTop / (docHeight - winHeight);
-  let scrollPercentRounded = Math.round(scrollPercent * 100);
-  document.title = `(${scrollPercentRounded}%) ${originalTitle}`;
-}, { passive: true });
+window.addEventListener(
+  "scroll",
+  () => {
+    const scrollTop = window.scrollY;
+    const containers = document.querySelectorAll(
+      "body,main,*[class*='content'],*[class*='Content']"
+    );
+    const main_container = containers[containers.length - 1];
+    const main_container_sizes = main_container.getBoundingClientRect();
+    let docHeight =
+      scrollTop + main_container_sizes.top + main_container_sizes.height;
+    let winHeight = window.innerHeight;
+    let scrollPercent = scrollTop / (docHeight - winHeight);
+    let scrollPercentRounded = Math.min(
+      Math.max(Math.round(scrollPercent * 100), 0),
+      100
+    );
+    document.title = `(${scrollPercentRounded}%) ${originalTitle}`;
+  },
+  { passive: true }
+);
