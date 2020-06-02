e6ed5bebde75e8d470028823f55fbe1a10622a12
Moshe Harush <mosheh@yad2.co.il>
2020-06-02 14:07:19 +0300

    Improve main content detection

--- src/extension.js
+++ src/extension.js
@@ -7,14 +7,25 @@ const originalTitle = document.title;
 window.addEventListener(
   "scroll",
   () => {
+    const windowHeight = document.body.offsetHeight;
     const scrollTop = window.scrollY;
     const containers = document.querySelectorAll(
-      "body,main,*[class*='content'],*[class*='Content']"
+      "body,main,*[class~='content'],*[class*='Content'],*[class~='article']:first-child,article:first-child"
     );
-    const main_container = containers[containers.length - 1];
-    const main_container_sizes = main_container.getBoundingClientRect();
+
+    let mainContainer = null,
+      mainContainerSizes = null;
+
+    for (let x = containers.length - 1; x >= 0; x--) {
+      mainContainer = containers[x];
+      mainContainerSizes = mainContainer.getBoundingClientRect();
+      if (windowHeight / mainContainerSizes.height < 3) {
+        break;
+      }
+    }
+
     let docHeight =
-      scrollTop + main_container_sizes.top + main_container_sizes.height;
+      scrollTop + mainContainerSizes.top + mainContainerSizes.height;
     let winHeight = window.innerHeight;
     let scrollPercent = scrollTop / (docHeight - winHeight);
     let scrollPercentRounded = Math.min(
