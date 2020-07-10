e1dc900f0d8f3868db5780458a151aa3ab1eff39
Moshe Harush <upload770@gmail.com>
2020-07-10 18:42:38 +0300

    Use Intersection api

--- src/extension.js
+++ src/extension.js
@@ -4,34 +4,50 @@ console.log("Scroll Percentage in Tab Title Extension loaded 👍");
 
 const originalTitle = document.title;
 
-window.addEventListener(
-  "scroll",
-  () => {
-    const windowHeight = window.innerHeight;
-    const scrollTop = window.scrollY;
-    const containers = document.querySelectorAll(
-      "body,main,*[class~='content'],*[class*='Content'],*[class~='article']:first-child,article:first-child"
-    );
+function findOutMainContentElement() {
+  const windowHeight = window.innerHeight;
+  const containers = document.querySelectorAll(
+    "body,main,*[class~='content'],*[class*='Content'],*[class~='article']:first-child,article:first-child"
+  );
 
-    let mainContainer = null,
-      mainContainerSizes = null;
+  let bestMatchedContainer = {
+    ratio: 0,
+    elm: null,
+  };
 
-    for (let x = containers.length - 1; x >= 0; x--) {
-      mainContainer = containers[x];
-      mainContainerSizes = mainContainer.getBoundingClientRect();
-      if (windowHeight / mainContainerSizes.height < 3) {
-        break;
-      }
-    }
+  for (let x = containers.length - 1; x >= 0; x--) {
+    const optionalMainContainer = containers[x];
+    const optionalMainContainerSizes = optionalMainContainer.getBoundingClientRect();
+    const containerRatio = windowHeight / optionalMainContainerSizes.height;
 
-    const docHeight =
-      scrollTop + mainContainerSizes.top + mainContainerSizes.height;
-    const scrollPercent = scrollTop / (docHeight - windowHeight);
-    const scrollPercentRounded = Math.min(
-      Math.max(Math.round(scrollPercent * 100), 0),
-      100
-    );
-    document.title = `(${scrollPercentRounded}%) ${originalTitle}`;
-  },
-  { passive: true }
-);
+    bestMatchedContainer.ratio <= containerRatio &&
+      (bestMatchedContainer = {
+        ratio: containerRatio,
+        elm: optionalMainContainer,
+        sizes: optionalMainContainerSizes,
+      });
+  }
+
+  return bestMatchedContainer;
+}
+
+let {
+  elm: mainContainer,
+  sizes: mainContainerSizes,
+} = findOutMainContentElement();
+
+let observer = new IntersectionObserver((entries, observer) => {
+  const scrollTop = window.scrollY;
+  let docHeight = mainContainerSizes.top + mainContainerSizes.height;
+  let winHeight = window.innerHeight;
+  let scrollPercent =
+    scrollTop > docHeight ? 100 : scrollTop / (docHeight - winHeight);
+  let scrollPercentRounded = Math.min(
+    Math.max(Math.round(scrollPercent * 100), 0),
+    100
+  );
+  document.title = `(${scrollPercentRounded}%) ${originalTitle}`;
+});
+mainContainer.querySelectorAll("p").forEach((elem) => {
+  observer.observe(elem);
+});
